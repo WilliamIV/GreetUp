ee106497cdfc580ef19330ca8d54cd085a0c7091
WilliamIV <william.green.iv@gmail.com>
2016-10-06 11:38:38 -0400

    initial commit

---
+++ Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GreetUp.Models
+{
+    public class Event
+    {
+        [Key]
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime Time { get; set; }
+        public string Description { get; set; }
+        public int RSVP { get; set; }
+    }
+}
